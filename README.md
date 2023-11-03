304b7e5ce2f7a1669e4c280dc675a82000f073a0
konumaru <konumaru1022@gmail.com>
2023-11-04 02:22:21 +0900

    [add] init files

---
+++ README.md
@@ -0,0 +1,1 @@
+# AWS Text Analysis API
---
+++ tests/lambda_function/test_hello_world.py
@@ -0,0 +1,12 @@
+import json
+
+from lambda_function.text_classification.app import lambda_handler
+
+
+def test_hello_world() -> None:
+    event = {"text": ""}
+    context = None
+    response = lambda_handler(event, context)
+
+    assert response["statusCode"] == 200
+    assert json.loads(response["body"]) == {"message": "Hello world!"}
